7d94d8ebdaaa7ac8102bf0796237366d7fb37418
Hasini E <hasini1530@gmail.com>
2024-12-15 12:08:12 +0530

    Add files via upload

---
+++ Lambda_Thread.java
@@ -0,0 +1,8 @@
+public class Lambda_Thread {
+    public static void main(String[] args) {
+        Runnable r=()->System.out.println("I am new from java8 version");
+        Thread t=new Thread(r);
+        t.start();
+    }
+    
+}
